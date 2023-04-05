34283081384cddef37c05f7dfcd69be08c21c705
Francesco Meloni <francesco.meloni03@icloud.com>
2023-04-05 15:44:31 +0200

    Esercizio completo

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+alert("Benvenuto, qui creeremo la tua Password");
+
+
+
+nome = prompt("Inserisci il tuo nome");
+console.log(`Il nome dell'utente è ${nome}`);
+
+cognome  = prompt("Inserisci il tuo cognome");
+console.log(`Il cognome dell'utente è ${cognome}`);
+
+colore  = prompt("Inserisci il tuo colore preferito");
+console.log(`Il colore preferito dell'utente è ${colore}`);
+
+document.writeln(`${nome}${cognome}${colore}2023`);
+
+
+
+
+
+
+
+
+
